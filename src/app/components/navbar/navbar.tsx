64095f91d74cb874f4f59f61c1c6cb33f46992bf
diarih <diarihza@gmail.com>
2023-09-19 21:06:57 +0700

    feat: navigation and pages

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,13 +1,21 @@
+'use client'
+
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
+
+    const pathname = usePathname()
+
+    const isHome = pathname === '/'
+
     return (
         <div className='flex justify-between'>
             <div className="tabs tabs-boxed w-fit">
-                <a className="tab tab-active">Menu</a>
-                <a className="tab">Order</a>
-                <a className="tab">Dapur</a>
-                <a className="tab">Kasir</a>
+                <a href='/' className={`tab ${pathname === '/' && 'tab-active'}`}>Menu</a>
+                <a href='/order' className={`tab ${pathname === '/order' && 'tab-active'}`}>Order</a>
+                <a href='/dapur' className={`tab ${pathname === '/dapur' && 'tab-active'}`}>Dapur</a>
+                <a href='/kasir' className={`tab ${pathname === '/kasir' && 'tab-active'}`}>Kasir</a>
             </div>
             <div>
                 <button className="btn btn-error">Reset</button>
