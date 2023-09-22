fd3c96aa84950fc9658c74577211db7f22e4b36d
diarih <diarihza@gmail.com>
2023-09-22 13:47:33 +0700

    feat: dapur

--- src/app/dapur/page.tsx
+++ src/app/dapur/page.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+'use client'
+
+import { MenuContext } from '@/context/MenuContext'
+import React, { useContext, useMemo } from 'react'
 
 export default function Dapur() {
+
+  const { order, menu } = useContext(MenuContext)
+
+  const orderMenu = () => {
+    const res = order?.map((e: any) => {
+      const [menuFilter] = menu.filter((x: any) => x.id == e.menuId)
+      const { menuId, ...res } = e
+      return {
+        ...res,
+        menu: {
+          ...menuFilter
+        }
+      }
+    })
+    return res
+  }
+
+  const data = useMemo(
+    () => orderMenu(),
+    [menu, order]
+  );
+
+  console.log(data)
+
   return (
-    <div>Dapur</div>
+    <div className="flex">
+      <div className="w-1/3 space-y-4">
+        <h3 className="font-semibold text-xl leading-none">Meja 1</h3>
+        <div className="space-y-1">
+          {
+            data?.filter((e: any) => e.tableId === '1').map((x: any, i: number) => {
+              return (
+                <div key={i} className="flex text-sm text-muted-foreground">
+                  <div className="w-[30px]">{x.qty}x</div>
+                  <div className="w-full">{x.menu.name}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className="w-1/3 space-y-4">
+        <h3 className="font-semibold text-xl leading-none">Meja 2</h3>
+        <div className="space-y-1">
+          {
+            data?.filter((e: any) => e.tableId === '2').map((x: any, i: number) => {
+              return (
+                <div key={i} className="flex text-sm text-muted-foreground">
+                  <div className="w-[30px]">{x.qty}x</div>
+                  <div className="w-full">{x.menu.name}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className="w-1/3 space-y-4">
+        <h3 className="font-semibold text-xl leading-none">Meja 3</h3>
+        <div className="space-y-1">
+          {
+            data?.filter((e: any) => e.tableId === '3').map((x: any, i: number) => {
+              return (
+                <div key={i} className="flex text-sm text-muted-foreground">
+                  <div className="w-[30px]">{x.qty}x</div>
+                  <div className="w-full">{x.menu.name}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
