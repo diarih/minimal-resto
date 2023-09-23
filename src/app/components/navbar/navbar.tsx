3aafa26fc91fe0a88d8562b53cbd7b985589d24e
diarih <diarihza@gmail.com>
2023-09-23 09:35:18 +0700

    fix: navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -3,19 +3,21 @@
 import React, { useContext } from 'react'
 import { usePathname } from 'next/navigation'
 import { MenuContext } from '@/context/MenuContext'
+import Link from 'next/link'
 
 export default function Navbar() {
 
     const pathname = usePathname()
     const { reset } = useContext(MenuContext)
+    
 
     return (
         <div className='flex justify-between'>
             <div className="tabs tabs-boxed w-fit">
-                <a href='/' className={`tab ${pathname === '/' && 'tab-active'}`}>Menu</a>
-                <a href='/order' className={`tab ${pathname === '/order' && 'tab-active'}`}>Order</a>
-                <a href='/dapur' className={`tab ${pathname === '/dapur' && 'tab-active'}`}>Dapur</a>
-                <a href='/kasir' className={`tab ${pathname === '/kasir' && 'tab-active'}`}>Kasir</a>
+                <Link href='/' className={`tab ${pathname === '/' && 'tab-active'}`}>Menu</Link>
+                <Link href='/order' className={`tab ${pathname === '/order' && 'tab-active'}`}>Order</Link>
+                <Link href='/dapur' className={`tab ${pathname === '/dapur' && 'tab-active'}`}>Dapur</Link>
+                <Link href='/kasir' className={`tab ${pathname === '/kasir' && 'tab-active'}`}>Kasir</Link>
             </div>
             <div>
                 <button onClick={() => reset()} className="btn btn-error">Reset</button>
